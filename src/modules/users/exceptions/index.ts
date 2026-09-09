aae2abecab710f353206114257cb5431396e8ce7
lucaaszsx <jlucasmelob558@gmail.com>
2026-09-09 16:27:06 -0300

    chore: remove barrel file from user exceptions
    just to keep it standardized

--- src/modules/users/exceptions/index.ts
+++
@@ -1,2 +0,0 @@
-export { UserAlreadyExistsException } from './user-already-exists.exception.js';
-export { UserNotFoundException } from './user-not-found.exception.js';
--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -2,7 +2,8 @@ import type { FindOptionsWhere, Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity.js';
-import { UserAlreadyExistsException, UserNotFoundException } from './exceptions/index.js';
+import { UserAlreadyExistsException } from './exceptions/user-already-exists.exception.js';
+import { UserNotFoundException } from './exceptions/user-not-found.exception.js';
 
 export interface CreateUserData {
     username: string;
