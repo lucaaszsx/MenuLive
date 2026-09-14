0b88de77c80efbde24756625813ee3a4679bcd8b
lucaaszsx <jlucasmelob558@gmail.com>
2026-09-14 16:44:15 -0300

    feat(auth): create login logic on auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,23 +1,31 @@
+import type { DataSource } from 'typeorm';
 import type { EnvConfig } from '#/config/env.js';
 import type { UserEntity } from '../users/entities/user.entity.js';
 import type { CreateUserInput } from '../users/user.service.js';
-import type { SessionEntity } from './entities/session.entity.js';
 import type { JwtTokenPayload } from './types/jwt-payload.type.js';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
-import { UserService } from '../users/user.service.js';
-import { InvalidCredentialsException } from './exceptions/invalid-credentials.exception.js';
+import { InjectDataSource } from '@nestjs/typeorm';
 import bcrypt from 'bcrypt';
+import ms from 'ms';
+import { UserService } from '../users/user.service.js';
+import { RefreshTokenEntity } from './entities/refresh-token.entity.js';
+import { SessionEntity } from './entities/session.entity.js';
+import { InvalidCredentialsException } from './exceptions/invalid-credentials.exception.js';
+import { createHash } from 'node:crypto';
 
 export interface LoginInput {
     username: string;
     password: string;
+    ipAddress: string;
+    userAgent: string;
 }
 
 @Injectable()
 export class AuthService {
     constructor(
+        @InjectDataSource() private readonly dataSource: DataSource,
         private readonly configService: ConfigService<EnvConfig>,
         private readonly userService: UserService,
         private readonly jwtService: JwtService
@@ -31,9 +39,41 @@ export class AuthService {
         const user = await this.userService.findWithPassword({ username: data.username });
         if (!user || !(await bcrypt.compare(data.password, user.password)))
             throw new InvalidCredentialsException();
+
+        return new Promise<{ refreshToken: string; accessToken: string }>(
+            async (resolve) => {
+                await this.dataSource.transaction(async (manager) => {
+                    const dbSession = manager.create(SessionEntity, {
+                        userId: user.id,
+                        userAgent: data.userAgent,
+                        ipAddress: data.ipAddress
+                    });
+                    await manager.save(SessionEntity, dbSession);
+
+                    const { hashedRefreshToken, refreshToken, accessToken } =
+                        await this.issueTokenPair(user, dbSession);
+                    const dbRefreshToken = manager.create(RefreshTokenEntity, {
+                        sessionId: dbSession.id,
+                        expiresAt: new Date(Date.now() + this.getRefreshTokenTtlMs()),
+                        token: hashedRefreshToken
+                    });
+                    await manager.save(RefreshTokenEntity, dbRefreshToken);
+
+                    resolve({ refreshToken, accessToken });
+                });
+            }
+        );
     }
 
-    public async issueTokenPair(user: UserEntity, session: SessionEntity) {
+    public getRefreshTokenTtlMs() {
+        return ms(
+            this.configService.getOrThrow('jwt.refreshTokenExpiresIn', {
+                infer: true
+            })
+        );
+    }
+
+    private async issueTokenPair(user: UserEntity, session: SessionEntity) {
         const payload: JwtTokenPayload = { uid: user.id, sid: session.id };
         const accessToken = await this.jwtService.signAsync(payload, {
             secret: this.configService.getOrThrow('jwt.accessTokenSecret', {
@@ -47,11 +87,13 @@ export class AuthService {
             secret: this.configService.getOrThrow('jwt.refreshTokenSecret', {
                 infer: true
             }),
-            expiresIn: this.configService.getOrThrow('jwt.refreshTokenExpiresIn', {
-                infer: true
-            })
+            expiresIn: this.getRefreshTokenTtlMs()
         });
 
-        return { refreshToken, accessToken };
+        return {
+            hashedRefreshToken: createHash('sha256').update(refreshToken).digest('hex'),
+            refreshToken,
+            accessToken
+        };
     }
 }
