261dd9973ea22c4f43c8d3b6496b3b47d3224723
lucaaszsx <jlucasmelob558@gmail.com>
2026-09-14 15:01:16 -0300

    chore(auth): update module for new additions

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '../users/user.module.js';
 import { AuthController } from './auth.controller.js';
 import { AuthService } from './auth.service.js';
 import { RefreshTokenEntity } from './entities/refresh-token.entity.js';
 import { SessionEntity } from './entities/session.entity.js';
+import { AccessTokenStrategy } from './strategies/access-token.strategy.js';
+import { RefreshTokenStrategy } from './strategies/refresh-token.strategy.js';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([SessionEntity, RefreshTokenEntity]), UserModule],
+    imports: [
+        TypeOrmModule.forFeature([SessionEntity, RefreshTokenEntity]),
+        JwtModule.register({}),
+        PassportModule,
+        UserModule
+    ],
     controllers: [AuthController],
-    providers: [AuthService]
+    providers: [AuthService, AccessTokenStrategy, RefreshTokenStrategy]
 })
 export class AuthModule {}
