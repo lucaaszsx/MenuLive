7c5e244dfcecacb703b16771c83e1f16813bd9bf
lucaaszsx <jlucasmelob558@gmail.com>
2026-09-14 14:59:00 -0300

    refactor(user): update user service finding methods

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -5,15 +5,14 @@ import { UserEntity } from './entities/user.entity.js';
 import { UserAlreadyExistsException } from './exceptions/user-already-exists.exception.js';
 import { UserNotFoundException } from './exceptions/user-not-found.exception.js';
 
-export interface CreateUserData {
+export interface CreateUserInput {
     username: string;
     password: string;
 }
 
-export interface FindOneUserOptions {
-    id?: string;
-    username?: string;
-}
+export type FindOneUserInput = { throwErrorOnNull?: boolean } & (
+    { id: string; username?: never } | { id?: never; username: string }
+);
 
 @Injectable()
 export class UserService {
@@ -22,7 +21,7 @@ export class UserService {
         private readonly userRepository: Repository<UserEntity>
     ) {}
 
-    public async createUser(data: CreateUserData) {
+    public async createUser(data: CreateUserInput) {
         if (await this.existsByUsername(data.username))
             throw new UserAlreadyExistsException();
 
@@ -30,24 +29,28 @@ export class UserService {
         return this.userRepository.save(user);
     }
 
-    public async findOne(options: FindOneUserOptions) {
+    public async findOne(options: FindOneUserInput) {
         const where: FindOptionsWhere<UserEntity>[] = [];
         if (options.id) where.push({ id: options.id });
         if (options.username) where.push({ username: options.username });
 
-        const user = await this.userRepository.find({ where });
-        if (!user) throw new UserNotFoundException();
+        const user = await this.userRepository.findOne({ where });
+        if (!user && options.throwErrorOnNull) throw new UserNotFoundException();
 
         return user;
     }
 
-    public async findWithPassword(username: string) {
-        const user = await this.userRepository
+    public async findWithPassword(options: FindOneUserInput) {
+        const query = await this.userRepository
             .createQueryBuilder('user')
-            .addSelect('user.password')
-            .where('user.username = :username', { username })
-            .getOne();
-        if (!user) throw new UserNotFoundException();
+            .addSelect('user.password');
+
+        if (options.id) query.orWhere('user.id = :id', { id: options.id });
+        if (options.username)
+            query.orWhere('user.username = :username', { username: options.username });
+
+        const user = query.getOne();
+        if (!user && options.throwErrorOnNull) throw new UserNotFoundException();
 
         return user;
     }
