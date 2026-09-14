0dcb8d5378b1a690014dd1d888480bf8d2251cba
lucaaszsx <jlucasmelob558@gmail.com>
2026-09-14 16:43:38 -0300

    feat(auth): create login logic

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,15 +1,23 @@
-import { Body, Controller, Get, Ip, Post } from '@nestjs/common';
+import type { Response } from 'express';
+import type { EnvConfig } from '#/config/env.js';
+import { Body, Controller, HttpCode, HttpStatus, Ip, Post, Res } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { COOKIE_REFRESH_TOKEN_NAME } from '#/common/constants.js';
 import { UserAgent } from '#/common/decorators/user-agent.decorator.js';
 import { AuthService } from './auth.service.js';
-import { CreateUserDTO, LoginDTO } from './dto/index.js';
+import { CreateUserDTO, LoginDTO, LoginResponseDTO } from './dto/index.js';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+    constructor(
+        private readonly configService: ConfigService<EnvConfig>,
+        private readonly authService: AuthService
+    ) {}
 
     @Post('/register')
-    public async register(@Body() data: CreateUserDTO) {
-        return this.authService.register({
+    @HttpCode(HttpStatus.CREATED)
+    public async register(@Body() data: CreateUserDTO): Promise<void> {
+        await this.authService.register({
             username: data.username,
             password: data.password
         });
@@ -17,8 +25,26 @@ export class AuthController {
 
     @Post('/login')
     public async login(
-        @Body() _data: LoginDTO,
-        @Ip() _ipAddress: string,
-        @UserAgent() _userAgent: string
-    ) {}
+        @Body() data: LoginDTO,
+        @Ip() ipAddress: string,
+        @UserAgent() userAgent: string,
+        @Res({ passthrough: true }) response: Response
+    ): Promise<LoginResponseDTO> {
+        const { refreshToken, accessToken } = await this.authService.login({
+            username: data.username,
+            password: data.password,
+            ipAddress,
+            userAgent
+        });
+
+        response.cookie(COOKIE_REFRESH_TOKEN_NAME, refreshToken, {
+            httpOnly: true,
+            secure: this.configService.getOrThrow('env') === 'production',
+            maxAge: this.authService.getRefreshTokenTtlMs(),
+            sameSite: 'lax',
+            path: '/'
+        });
+
+        return LoginResponseDTO.from({ accessToken });
+    }
 }
