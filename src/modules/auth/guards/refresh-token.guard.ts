3ad33013c5006119c689351eb0a0873137019e92
lucaaszsx <jlucasmelob558@gmail.com>
2026-09-17 13:04:50 -0300

    fix(auth): fix validation when refresh token is expired

--- src/modules/auth/guards/refresh-token.guard.ts
+++ src/modules/auth/guards/refresh-token.guard.ts
@@ -6,8 +6,8 @@ import { UnauthorizedException } from '../exceptions/unauthorized.exception.js';
 
 @Injectable()
 export class RefreshTokenGuard extends AuthGuard('jwt-refresh') {
-    override handleRequest<TUser = JwtTokenPayload>(err: any, user: TUser): TUser {
-        if (err instanceof TokenExpiredError)
+    override handleRequest<TUser = JwtTokenPayload>(err: any, user: TUser, info: any): TUser {
+        if (info instanceof TokenExpiredError)
             throw new UnauthorizedException(['Refresh token expired']);
         if (err && !user) throw new UnauthorizedException(['Invalid refresh token']);
         return user;
